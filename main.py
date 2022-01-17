560b7724d86db7ffa2c443c3de823fea3f04c101
Lenar Zakirov <sciner@users.noreply.github.com>
2022-01-17 12:07:34 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from pydub import AudioSegment
+import json
+import os
+from io import StringIO
+
+def get_name_no_ext(filename):
+    pathname, extension = os.path.splitext(filename)
+    filename = pathname.split('/')
+    return filename[-1]
+
+filelist = os.listdir("./sounds")
+
+offset = 0
+sprite = dict()
+sound_final = None
+for file in filelist:
+    print(file)
+    if '.ogg' == file[-4:]:
+        sound = AudioSegment.from_ogg("../" + file)
+    elif '.mp3' == file[-4:]:
+        sound = AudioSegment.from_mp3("../" + file)
+    else:
+        continue
+    if not sound_final:
+        sound_final = sound
+    else:
+        sound_final = sound_final + sound
+    sound_len = len(sound)
+    sprite[get_name_no_ext(file)] = [offset, sound_len - 5]
+    offset = offset + sound_len
+sound_final.export("./sprite.ogg", format="ogg")
+sound_final.export("./sprite.mp3", format="mp3")
+io = StringIO()
+final_result = dict()
+final_result["src"] = ["/sprite.ogg", "/sprite.mp3"]
+final_result["sprite"] = sprite
+
+with open('sprite.json', 'w') as f:
+    json.dump(final_result, f, indent = 6)
+    
+print(io.getvalue())
